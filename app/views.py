710241376f236c8bc0c4cb222cf6b2153579e42c
carbohydratepro <carbohydratepro@gmail.com>
2024-08-19 00:43:30 +0900

    投稿一覧画面にグラフを追加

--- app/views.py
+++ app/views.py
@@ -1,13 +1,91 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.db.models import Sum, F, Q
+from django.utils.timezone import make_aware
 from .forms import TransactionForm, PaymentMethodForm, CategoryForm
 from .models import Transaction, PaymentMethod, Category
+from datetime import datetime, timedelta
+
+import json
 
 @login_required
 def index(request):
-    transactions = Transaction.objects.filter(user=request.user)  # ユーザーに紐づくデータのみ表示
-    return render(request, 'app/index.html', {'transactions': transactions})
+    target_date = request.GET.get('target_date', None)
 
+    if target_date:
+        target_date = make_aware(datetime.strptime(target_date, '%Y-%m'))
+    else:
+        # target_dateが指定されていない場合、現在の月をデフォルトとして設定
+        target_date = make_aware(datetime.now().replace(day=1))
+
+    start_date = target_date.replace(day=1)
+    end_date = (start_date + timedelta(days=32)).replace(day=1) - timedelta(days=1)
+
+    transactions = Transaction.objects.filter(user=request.user, date__range=(start_date, end_date))
+
+    # 日付範囲の作成（固定された月初から月末）
+    date_range = [(start_date + timedelta(days=i)).strftime('%Y-%m-%d') for i in range((end_date - start_date).days + 1)]
+
+    # グラフ用データの作成
+    category_data = transactions.values('category__name').annotate(total=Sum('amount')).order_by('-total')
+    major_category_data = transactions.values('major_category').annotate(total=Sum('amount')).order_by('-total')
+
+    expense_data = []
+    balance_data = []
+    current_balance = 0
+
+    for date in date_range:
+        daily_expense = transactions.filter(date__date=date, transaction_type='expense').aggregate(Sum('amount'))['amount__sum'] or 0
+        daily_income = transactions.filter(date__date=date, transaction_type='income').aggregate(Sum('amount'))['amount__sum'] or 0
+        current_balance += float(daily_income) - float(daily_expense) # Decimalをfloatに変換
+        expense_data.append(float(daily_expense)) # Decimalをfloatに変換
+        balance_data.append(float(current_balance)) # Decimalをfloatに変換
+    
+    category_data_json = json.dumps({
+        'labels': [entry['category__name'] for entry in category_data],
+        'datasets': [{
+            'data': [float(entry['total']) for entry in category_data],  # Decimalをfloatに変換
+            'backgroundColor': ['#FF6384', '#36A2EB', '#FFCE56'],
+        }]
+    })
+
+    major_category_data_json = json.dumps({
+        'labels': [entry['major_category'] for entry in major_category_data],
+        'datasets': [{
+            'data': [float(entry['total']) for entry in major_category_data],  # Decimalをfloatに変換
+            'backgroundColor': ['#4BC0C0', '#FF9F40', '#9966FF'],
+        }]
+    })
+
+    expense_data_json = json.dumps({
+        'labels': date_range,
+        'datasets': [{
+            'label': '支出',
+            'data': expense_data,
+            'backgroundColor': '#FF6384',
+        }]
+    })
+
+    balance_data_json = json.dumps({
+        'labels': date_range,
+        'datasets': [{
+            'label': '所持金',
+            'data': balance_data,
+            'fill': False,
+            'borderColor': '#36A2EB',
+        }]
+    })
+
+    return render(request, 'app/index.html', {
+        'transactions': transactions,
+        'category_data_json': category_data_json,
+        'major_category_data_json': major_category_data_json,
+        'expense_data_json': expense_data_json,
+        'balance_data_json': balance_data_json,
+        'default_target_date': target_date.strftime('%Y-%m')  # 今日の日付をテンプレートに渡す
+    })
+
+    
 @login_required
 def create_transaction(request):
     if request.method == 'POST':
